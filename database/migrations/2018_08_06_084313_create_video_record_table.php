cbb2a7ad25283b9d957097462bafa5ac464143eb
KevinHo <kevinho2018@ifalo.com.tw>
2018-08-06 17:43:48 +0800

    Add migration create table BaccaratHistory and VideoRecord

--- database/migrations/2018_08_06_084313_create_video_record_table.php
+++ database/migrations/2018_08_06_084313_create_video_record_table.php
@@ -7,15 +7,28 @@ use Illuminate\Database\Migrations\Migration;
 class CreateVideoRecordTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+* Run the migrations.
+*
+* @return void
+*/
     public function up()
     {
-        Schema::create('video_record', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+        Schema::create('VideoRecord', function (Blueprint $table) {
+            $table->increments('RecordId');
+            $table->string('TableId', 2)
+                ->comment('桌號');
+            $table->unsignedInteger('Round')
+                ->comment('輪號');
+            $table->unsignedInteger('Run')
+                ->comment('局號');
+            $table->dateTime('StartTime')
+                ->comment('牌局開始時間');
+            $table->string('FilePath', 255)
+                ->comment('影片S3存檔路徑');
+
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->index(['TableId', 'Round', 'Run']);
         });
     }
 
@@ -26,6 +39,6 @@ class CreateVideoRecordTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('video_record');
+        Schema::dropIfExists('VideoRecord');
     }
 }
